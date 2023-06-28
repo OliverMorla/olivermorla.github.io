6b8f558b0e3c16e3d723fadcbae8746911e75667
Oliver Morla <73266650+OliverMorla@users.noreply.github.com>
2023-06-28 15:14:24 -0400

    fixed buttons on portfolio page

--- src/pages/portfolio/index.jsx
+++ src/pages/portfolio/index.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faGithub, prefix } from "@fortawesome/free-brands-svg-icons";
 import { faGlobe } from "@fortawesome/free-solid-svg-icons";
 import { motion } from "framer-motion";
 import "./style.scss"
@@ -35,20 +35,33 @@ const Portfolio = () => {
             {!loading && <h2 className="ma-8"> Count: {data.length}</h2>}
             {!loading && data?.map((repo, index) => {
                 return (
-                    <a href={repo.html_url} key={repo?.id} target="_blank">
+                    <a href={repo?.html_url} key={repo?.id} target="_blank">
                         <motion.div className="repo-card"
                             initial={{ opacity: 0, x: 40 }}
-                            animate={{ opacity: 1, x: 0, transition: { delay: .2, duration: .2 * index, staggerChildren: 0.2, delayChildren: 0.4 } }}
+                            animate={{
+                                opacity: 1,
+                                x: 0,
+                                transition: {
+                                    delay: .2,
+                                    duration: .2 * index,
+                                    staggerChildren: 0.2,
+                                    delayChildren: 0.4
+                                }
+                            }}
                         >
                             <h1> {repo?.name} </h1>
                             <p> {repo?.description} </p>
                             <div className="btn-w">
-                                <button id="preview-btn">
-                                    <FontAwesomeIcon icon={faGlobe} /> Demo
-                                </button>
-                                <button id="src-code-btn">
-                                    <FontAwesomeIcon icon={faGithub} /> Source Code
-                                </button>
+                                <a href={repo?.homepage} target="_target">
+                                    <button id="preview-btn">
+                                        <FontAwesomeIcon icon={faGlobe} /> Demo
+                                    </button>
+                                </a>
+                                <a href={repo?.html_url}>
+                                    <button id="src-code-btn">
+                                        <FontAwesomeIcon icon={faGithub} /> Source Code
+                                    </button>
+                                </a>
                             </div>
                         </motion.div>
                     </a>
