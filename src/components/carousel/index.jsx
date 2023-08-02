c80a09ca7293caa50220b69e1a411d6a72332340
Oliver Morla <73266650+OliverMorla@users.noreply.github.com>
2023-08-02 10:22:22 -0400

    added carousel for to preview projects

--- src/components/carousel/index.jsx
+++ src/components/carousel/index.jsx
@@ -3,24 +3,24 @@ import "react-image-gallery/styles/css/image-gallery.css";
 
 const images = [
     {
-        original: "/public/assets/portfolio/airbnb-clone-2.webp",
-        thumbnail: "/public/assets/portfolio/airbnb-clone-2.webp",
+        original: "/assets/portfolio/airbnb-clone-2.webp",
+        thumbnail: "/assets/portfolio/airbnb-clone-2.webp",
     },
     {
-        original: "/public/assets/portfolio/urban-outfitters-clone.webp",
-        thumbnail: "/public/assets/portfolio/urban-outfitters-clone.webp",
+        original: "/assets/portfolio/urban-outfitters-clone.webp",
+        thumbnail: "/assets/portfolio/urban-outfitters-clone.webp",
     },
     {
-        original: "/public/assets/portfolio/nygeneralrenovation.webp",
-        thumbnail: "/public/assets/portfolio/nygeneralrenovation.webp",
+        original: "/assets/portfolio/nygeneralrenovation.webp",
+        thumbnail: "/assets/portfolio/nygeneralrenovation.webp",
     },
     {
-        original: "/public/assets/portfolio/blog-app.webp",
-        thumbnail: "/public/assets/portfolio/blog-app.webp",
+        original: "/assets/portfolio/blog-app.webp",
+        thumbnail: "/assets/portfolio/blog-app.webp",
     },
     {
-        original: "/public/assets/portfolio/portfolio.webp",
-        thumbnail: "/public/assets/portfolio/portfolio.webp",
+        original: "/assets/portfolio/portfolio.webp",
+        thumbnail: "/assets/portfolio/portfolio.webp",
     },
 ];
 
